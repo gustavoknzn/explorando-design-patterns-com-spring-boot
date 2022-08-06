ca1ca6426d3287bcc6a9d8e73fd3d06aa2d70ecb
gustavoknzn <gustavofkonzen@gmail.com>
2022-08-06 01:48:56 -0300

    Criação do projeto com SpringBoot

---
+++ src/main/java/br/com/dio/gof/service/ClienteService.java
@@ -0,0 +1,17 @@
+package br.com.dio.gof.service;
+
+import br.com.dio.gof.model.Cliente;
+
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+
+}
